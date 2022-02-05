8c36b40b5b6ac26c5910f27a71a6caf1868fd61b
Higor Andrade <48871533+higorandrade@users.noreply.github.com>
2022-02-05 20:27:15 +0000

    Commit 12

--- Movie.java
+++ Movie.java
@@ -36,24 +36,8 @@ public class Movie {
       return _title;
   }
 
-  public double getCharge(int daysRented){
-    double thisAmount = 0;
-    switch (getPriceCode()) {
-      case Movie.REGULAR:
-        thisAmount += 2;
-        if (daysRented > 2)
-          thisAmount += (daysRented - 2) * 1.5;
-        break;
-      case Movie.NEW_RELEASE:
-        thisAmount += daysRented * 3;
-        break;
-      case Movie.CHILDRENS:
-        thisAmount += 1.5;
-        if (daysRented > 3)
-          thisAmount += (daysRented - 3) * 1.5;
-        break;
-    }
-    return thisAmount;
+  public double getCharge(int daysRented) {
+    return _price.getCharge(daysRented);
   }
 
   public int getFrequentRenterPoints(int daysRented){
--- Price.java
+++ Price.java
@@ -1,3 +1,23 @@
 public abstract class Price {
-   public abstract int getPriceCode();
+  public abstract int getPriceCode();
+
+  public double getCharge(int daysRented) {
+    double thisAmount = 0;
+    switch (getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (daysRented > 2)
+          thisAmount += (daysRented - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += daysRented * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (daysRented > 3)
+          thisAmount += (daysRented - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
 }
