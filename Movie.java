6b77afb640f839441cdd9975ae1e84f66917f750
Higor Andrade <48871533+higorandrade@users.noreply.github.com>
2022-02-05 20:37:04 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -41,9 +41,6 @@ public class Movie {
   }
 
   public int getFrequentRenterPoints(int daysRented){
-    // add bonus for a two day new release rental
-    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-        return 2;
-    return 1;
+    return _price.getFrequentRenterPoints(daysRented);
   }
 }
--- Price.java
+++ Price.java
@@ -2,4 +2,11 @@ public abstract class Price {
   public abstract int getPriceCode();
 
   public abstract double getCharge(int daysRented);
+
+  public int getFrequentRenterPoints(int daysRented) {
+    // add bonus for a two day new release rental
+    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+        return 2;
+    return 1;
+  }
 }
