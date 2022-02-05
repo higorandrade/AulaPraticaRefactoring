fb95f4aeec7763248d42e8a2fc898b8152aa5078
Higor Andrade <48871533+higorandrade@users.noreply.github.com>
2022-02-05 18:55:41 +0000

    Commit 6

--- Customer.java
+++ Customer.java
@@ -23,18 +23,14 @@ public class Customer {
     Enumeration rentals = _rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
     while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
+      Rental each = (Rental) rentals.nextElement();
 
-        // add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-          each.getDaysRented() > 1) frequentRenterPoints ++;
-
-        // show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf
+      // add frequent renter points
+      frequentRenterPoints += each.getFrequentRenterPoints();
+      // show figures for this rental
+      result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf
           (each.getCharge()) + "\n";
-        totalAmount += each.getCharge();
+        totalAmount += each.getCharge();      
     }
     
     // add footer lines
--- Rental.java
+++ Rental.java
@@ -1,38 +1,45 @@
 public class Rental {
 
-   private Movie _movie;
-   private int _daysRented;
+  private Movie _movie;
+  private int _daysRented;
 
-   public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
-   }
+  public Rental(Movie movie, int daysRented) {
+    _movie = movie;
+    _daysRented = daysRented;
+  }
 
-   public int getDaysRented() {
-      return _daysRented;
-   }
+  public int getDaysRented() {
+    return _daysRented;
+  }
 
-   public Movie getMovie() {
-      return _movie;
-   }
+  public Movie getMovie() {
+    return _movie;
+  }
 
-   public double getCharge() {
-      double thisAmount = 0;
-      switch (getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (getDaysRented() > 2)
-            thisAmount += (getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (getDaysRented() > 3)
-            thisAmount += (getDaysRented() - 3) * 1.5;
-          break;
-      }
-      return thisAmount;
-   }
+  public double getCharge() {
+    double thisAmount = 0;
+    switch (getMovie().getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (getDaysRented() > 2)
+          thisAmount += (getDaysRented() - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += getDaysRented() * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (getDaysRented() > 3)
+          thisAmount += (getDaysRented() - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
+
+  public int getFrequentRenterPoints() { 
+    // add bonus for a two day new release rental
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+      return 2;
+    return 1;
+  }
 }
