0aff0da76cf295efa21ca33a518fb57614dd5977
Higor Andrade <48871533+higorandrade@users.noreply.github.com>
2022-02-05 20:40:19 +0000

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -1,9 +1,13 @@
 public class NewReleasePrice extends Price {
-   public int getPriceCode() {
-       return Movie.NEW_RELEASE;
-   }
+  public int getPriceCode() {
+    return Movie.NEW_RELEASE;
+  }
 
-   public double getCharge(int daysRented){
-      return daysRented * 3;
-   } 
+  public double getCharge(int daysRented){
+    return daysRented * 3;
+  }
+
+  public int getFrequentRenterPoints(int daysRented) {
+    return (daysRented > 1) ? 2: 1;
+  }
 }
